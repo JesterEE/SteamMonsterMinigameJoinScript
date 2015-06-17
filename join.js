c809136f356a6a0989fb492845e4ef6ea7b76c06
JesterEE <jesteree.login@soodonims.com>
2015-06-17 02:29:20 -0400

    Update join.js
    Fixed input box keydown error. $J.POST -> GM_xmlhttpRequest, added delay for sending subsequent requests. POST to leave current game giving Bad request 400 error response.  Not sure why.

--- join.js
+++ join.js
@@ -2,20 +2,20 @@
 // @name JesterEE's Steam Monster Minigame JoinGame Script
 // @namespace  https://github.com/jesteree/SteamMonsterMinigameJoinScript
 // @description  Adds a dialog box to the Steam Monster Minigame to join a game of your choice.
-// @version    1.2dev
-// @match      http://steamcommunity.com/minigame/*
+// @version    1.3dev
+// @match      http://steamcommunity.com/minigame/
+// @match      http://steamcommunity.com/minigame
 // @copyright  2015, JesterEE
 // @author  JesterEE
 // @grant unsafeWindow
-// @require https://fgnass.github.io/spin.js/spin.js
+// @grant GM_xmlhttpRequest
 // @updateURL https://raw.githubusercontent.com/jesteree/SteamMonsterMinigameJoinScript/dev/join.js
 // @downloadURL https://raw.githubusercontent.com/jesteree/SteamMonsterMinigameJoinScript/dev/join.js
 // ==/UserScript==
 
-
 // Options
 
-var delay = 1000; // in milliseconds 
+var delay = 5000; // in milliseconds 
 
 ////////////////////////////////////////////////////////////////////////////////
 // CSS
@@ -63,49 +63,95 @@ addGlobalStyle(
 
 // Do bad things: http://www.codedisqus.com/CyVkUkVqPX/how-to-call-a-greasemonkey-function-when-a-javascript-link-is-clicked.html
 // Hijack the JoinGame() function
+
 var JoinGame_orig = unsafeWindow.JoinGame;
 unsafeWindow.JoinGame = function() {
     JoinThisGame();
 };
 
+/*
+var JoinGameHelper_orig = unsafeWindow.JoinGameHelper;
+unsafeWindow.JoinGameHelper = function() {
+    JoinThisGameHelper();
+};
+*/
+
 function JoinThisGame() {
+    var cur_gameid = document.documentElement.outerHTML.match(RegExp(/'gameid' : '(\d+)'/))[1];
+    var cur_g_sessionID = document.documentElement.outerHTML.match(RegExp(/g_sessionID = "(\w+)"/))[1];
+    var data = {gameid: cur_gameid, sessionid: cur_g_sessionID};
+    var serialdata = JSON.stringify(data);
+    var gameid = document.getElementsByName('gameid_input')[0].value;
+    gameid = gameid.replace(/\D/g,'');
+    console.log('data: ' + data + ' JSON: ' + serialdata);
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: "http://steamcommunity.com/minigame/ajaxleavegame/",
+        data: serialdata,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        onload: function(json) {
+            console.log(json);
+            console.log('Going to the helper');
+            JoinThisGameHelper( gameid );
+        }
+    });
+}
+
+function JoinThisGameHelper( gameid ) {
     // Removes some errors of the original JoinGame and tries to join again on those occurrences.
     // Removes all non-numeric characters from the Game ID to prevent typing mistakes/inaccuracies.
     // If nothing is entered in the Game ID dialog, the default behavior is preserved.
-    var gameid = document.getElementsByName('gameid_input')[0].value;
-    gameid = gameid.replace(/\D/g,'');
+    var data = {gameid: gameid};
+    var serialdata = JSON.stringify(data);
+    console.log('data: ' + data + ' JSON: ' + serialdata);
     document.getElementsByName('gameid_input')[0].value = gameid;
-    $J.post('http://steamcommunity.com/minigame/ajaxjoingame/',
-            { 'gameid' : gameid }
-            ).done( function( json ) {
-                if ( json.success == '1' ) {
-                    top.location.href = 'http://steamcommunity.com/minigame/towerattack/';
-                }
-                else {
-                    setTimeout(function(){
-                        coonsole.log('join.js: Probably a garbage packet.  Trying again in' + delay);
-                    }, delay);
-                    JoinThisGame( gameid );
-                }
+    console.log('Joining GameID: ' + gameid);
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: "http://steamcommunity.com/minigame/ajaxjoingame/",
+        data: serialdata,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        onload: function(json) {
+            console.log(json);
+            var responseJSON = json.responseText.evalJSON();
+            if ( responseJSON.success == '1' ) {
+                top.location.href = 'http://steamcommunity.com/minigame/towerattack/';
             }
-        	).fail( function( jqXHR )
-                   {
-                       var responseJSON = jqXHR.responseText.evalJSON();
-                       if ( responseJSON.success == '24' && responseJSON.errorMsg ) {
-				           ShowAlertDialog( 'Error', responseJSON.errorMsg ); // Usually for the 10 lvl restriction
-    			       }
-	    		       else if ( responseJSON.success == '25' ) {
-		    		       ShowAlertDialog( 'Error', 'There was a problem trying to join the game: it already has the maximum number of players.' );
-			           }
-    			       else {
-                           setTimeout(function(){
-                               coonsole.log('join.js: Probably a bad request.  Trying again in' + delay);
-                           }, delay);
-	    			       JoinThisGame( gameid );
-		    	       }
-		           }
-            );
+            else {
+                setTimeout(function(){
+                    JoinThisGameHelper( gameid );
+                }, delay);
+                console.log('join.js: Probably a garbage packet.  Trying again in ' + delay + 'ms');
+            }
+        },
+        onerror: function( jqXHR ) {
+            console.log(jqXHR);
+            var responseJSON = jqXHR.responseText.evalJSON();
+            if ( responseJSON.success == '24' && responseJSON.errorMsg ) {
+                ShowAlertDialog( 'Error', responseJSON.errorMsg ); // Usually for the 10 lvl restriction
+            }
+            else if ( responseJSON.success == '25' ) {
+                ShowAlertDialog( 'Error', 'There was a problem trying to join the game: it already has the maximum number of players.' );
+            }
+            else {
+                setTimeout(function(){
+                    JoinThisGameHelper( gameid );
+                }, delay);
+                console.log('join.js: Probably a bad request.  Trying again in ' + delay + 'ms');
+		    }
+		}
+    });
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// Future - Working status
+////////////////////////////////////////////////////////////////////////////////
+
+// @require https://fgnass.github.io/spin.js/spin.js
 
 // Spinner Settings
 /*
@@ -135,6 +181,7 @@ var spinner = new Spinner().spin();
 target.appendChild(spinner.el);
 */
 
+
 ////////////////////////////////////////////////////////////////////////////////
 // Update the landing page HTML
 ////////////////////////////////////////////////////////////////////////////////
@@ -143,21 +190,18 @@ var game_div;
 try {
     game_div = document.getElementsByClassName('current_game')[0].children[0];
     game_div.outerHTML = '<span class="gameidcss">Game ID: <input name="gameid_input" class="gameid2css" type="text" value=""></span>' +
-//        + target.appendChild(spinner.el) +
         '</br><a href="javascript:JoinGame();" class="main_btn"><span>Play Sucka!</span>' +
         '<a><p class="start_new">or, <a href="javascript:StartNewGame();">start a new game</a></p>';
 }
 catch(err) {
         game_div = document.getElementsByClassName('new_game')[0].children[0];
         game_div.outerHTML = '<span class="gameidcss">Game ID: <input name="gameid_input" class="gameid2css" type="text" value=""></span>' +
-//            + target.appendChild(spinner.el) +
             '</br><a href="javascript:JoinGame();" class="main_btn"><span>Play Sucka!</span><a>';
 }
 
-// Add a listener for an enter key 
+// Add a listener for an enter key
 document.getElementsByName("gameid_input")[0].addEventListener("keydown", function(e) {
     if (!e) { var e = window.event; }
-    //e.preventDefault(); // sometimes useful
 
     // Enter is pressed
     if (e.keyCode == 13) { JoinThisGame(); }
